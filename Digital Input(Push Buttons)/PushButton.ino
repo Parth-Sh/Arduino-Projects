faecbb4fb78294b65518d9e966725aad7b7e5d88
Parth Sharma <learningparth@gmail.com>
2025-07-28 02:49:37 +0530

    Update PushButton.ino

--- Digital Input(Push Buttons)/PushButton.ino
+++ Digital Input(Push Buttons)/PushButton.ino
@@ -5,18 +5,18 @@ void setup() {
 // put your setup code here, to run once:
   pinMode(LEDPIN,OUTPUT);
   pinMode(buttonApin,INPUT_PULLUP);
-	pinMode(buttonBpin,INPUT_PULLUP);
+  pinMode(buttonBpin,INPUT_PULLUP);
  }
  	
  void loop() {
  // put your main code here, to run repeatedly:
   if (digitalRead(buttonApin)==LOW)
  { 
- 	    digitalWrite(LEDPIN,HIGH);
- 	 }
- 	 if (digitalRead(buttonBpin)==LOW)
- 	 {
- 	    digitalWrite(LEDPIN,LOW);
- 	 }
- 	}
+      digitalWrite(LEDPIN,HIGH);
+ }
+  if (digitalRead(buttonBpin)==LOW)
+ {
+      digitalWrite(LEDPIN,LOW);
+ }
+   }
  	
